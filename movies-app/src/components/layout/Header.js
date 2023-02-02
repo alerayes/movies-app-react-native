75cd43ad3eec33a8659cd8872a1131d7b9ea9913
ALESSANDRA RAYESFORTE <alerayes@hotmail.com>
2023-02-01 20:29:51 -0800

    create header component

---
+++ movies-app/src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import { StatusBar, Box, HStack, Text } from "native-base"
+
+const Header = () => {
+    return (
+        <>
+            <StatusBar backgroundColor='hotpink' barStyle='light-content'/>
+            <Box safeAreaTop backgroundColor='hotpink'>
+                <HStack bg='hotpink' px={1} py={3} alignItems='center' justifyContent='center'>
+                    <Text color='#fff' fontSize={20} fontWeight='bold'>
+                        Recipe App
+                    </Text>
+                </HStack>
+            </Box>
+        </>    
+    )
+}
+
+export default Header
